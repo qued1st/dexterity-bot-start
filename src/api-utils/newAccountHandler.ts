40b0a034ebc70e2fd69b77075bd78799a34a504a
qued1st <queshalom3@gmail.com>
2024-03-09 05:38:47 +0100

    telegram-trading-bot-2

--- src/api-utils/newAccountHandler.ts
+++ src/api-utils/newAccountHandler.ts
@@ -2,5 +2,26 @@ const HeliusApiKey = "";
 const WebHookId = "";
 
 export const newAcccountSubscriptionHandler = async (newTrg: string) => {
-
+    try {
+        const response = await fetch(`https://api.helius.xyz/v0/webhooks/${WebHookId}?api-key=${HeliusApiKey}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              webhookURL: "https://trades-webhook-url.com/webhook",
+              transactionTypes: ["Any"],
+              accountAddresses: [newTrg],
+              webhookType: "rawDevnet",
+            }),
+          }
+        );
+      
+        const data = await response.json();
+        console.log({ data });
+      
+      } catch (e) {
+        console.error("error", e);
+     }
 };
--- src/api-utils/subscritionHandler.ts
+++ src/api-utils/subscritionHandler.ts
@@ -8,11 +8,62 @@ export const handleNewSubscription = async (
   newTrg: string | null,
   AppState: Map<string, any>
 ) => {
+  if (newTrg == null) {
+    return new Response(JSON.stringify({ error: "No trg was passed" }), {
+      status: 500,
+    });
+  }
 
+  const copiedTrader = new Trader(manifest, new PublicKey(newTrg), true);
+  AppState.set("copiedTrader", copiedTrader);
+
+  await newAcccountSubscriptionHandler(newTrg)
+
+  return new Response(
+    JSON.stringify({
+      ok: "Successfully set new Trader to Copy",
+      newTrg,
+    }),
+    { status: 200 }
+  );
 };
 
 export const handleCancelSubscription = async (
   AppState: Map<string, any>
 ) => {
+  AppState.delete("copiedTrader");
+  return new Response(
+    JSON.stringify({
+      ok: "Successfully canceled trader subscription",
+    }),
+    { status: 200 }
+  );
 
 };
+
+const server = Bun.serve({
+  async fetch(req, server) {
+    const url = new URL(req.url);
+    const { pathname, searchParams } = url;
+
+    let response: Response | undefined = new Response(
+      JSON.stringify({ status: 200 })
+    );
+
+    switch (pathname) {
+      case "/process-trade":
+        break;
+      case "/new-subscription":
+        response = await handleNewSubscription(trader, manifest, searchParams.get("trg"), AppState)
+        break;
+      case "/cancel-subscription":
+        response = await handleCancelSubscription(AppState)
+        break;
+      default:
+        break;
+    }
+
+    if (!response) return new Response(JSON.stringify({ status: 200 }));
+    return response;
+  },
+});
